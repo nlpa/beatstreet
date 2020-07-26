e1d529543cb7ae6a8ce1f61a6c671b38cf88ea40
Natalie Lampa <nlampa@umich.edu>
2020-07-26 10:26:17 -0500

    1.0

---
+++ Beat Road 1.0/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Beat Road 1.0
+//
+//  Created by Apple on 7/25/20.
+//  Copyright © 2020 Jorge Angel. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+//---------------------------------
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+//---------------------------------
+//FUNCTIONS/ACTIONS
+    
+
+@IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {} // func used to revert setting screen to menu
+    
+    
+    
+    
+    
+    
+    
+    
+
+}// [CLASS VIEW CONTROLLER END]
+
